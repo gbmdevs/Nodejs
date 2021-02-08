ed4b510f1ca5961534a48e206feb6837eb16ac6a
Guilherme B.(gbmdevs) <guih.smi2@outlook.com>
2021-02-08 16:34:19 +0000

    Conexao com Mongodb

--- helloMongo/index.js
+++ helloMongo/index.js
@@ -3,7 +3,21 @@ const mongoose = require('mongoose');
 const app      = express();
 const PORT     = 3000;
 
-mongoose.connect('mongodb://mongodb:27017/test', {useNewUrlParser: true});
+const Schema        = mongoose.Schema;
+const nomeColecao   = 'nomecerto';
+const colecaoSchema = new Schema({
+    chave1: {type: String} ,
+    chave2: {type: Number} , 
+});
+
+mongoose.connect('mongodb://mongodb:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const novoModelo  = mongoose.model('nomecerto', colecaoSchema, nomeColecao);
+const newDocument = new novoModelo({chave1: 'Oi', chave2: 12345});
+newDocument.save();
 
 app.get('/', (req,res) =>{
     res.send("E noiis!");
