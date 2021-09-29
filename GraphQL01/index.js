cd2b5e8d364ea387b6f28496ce876c8936b04dc2
Guilherme B.(gbmdevs) <guih.smi2@outlook.com>
2021-09-29 22:31:42 +0000

    Estudos Mongo e Graph

--- GraphQL01/index.js
+++ GraphQL01/index.js
@@ -3,11 +3,17 @@ const bodyParser        = require("body-parser");
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema}    = require("graphql");
 const app               = express();
+const mongoose          = require('mongoose');
+
+const Event             = require('./models/event.js');
 
 // Bacalhau para Testes
 const events = [];
 
 app.use(bodyParser.json());
+
+// Mutation - CREATE, DELETE,UPDATE
+// Query    - READ
 
 app.use('/graphql',
    graphqlHTTP({
@@ -42,23 +48,51 @@ app.use('/graphql',
      `),
      rootValue: {
         events: () => {
-            return events;
+           return Event.find()
+                .then(events => {
+                  return events.map(event => {
+                     return { ...event._doc , _id: event.id};
+                });                
+            }).catch(err => {
+                console.log(err);
+            })
         },
         createEvent: args => {
-            const event = {
-                _id: Math.random().toString(),
+            //const event = {
+            //    _id: Math.random().toString(),
+            //    title: args.eventInput.title,
+            //     description: args.eventInput.description,
+            //    price: +args.eventInput.price,
+            //     date: new Date().toISOString()
+            //}
+            const event  = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: new Date().toISOString()
-            }
-            events.push(event);
-            return event
+                date: new Date(args.eventInput.date)
+            })
+            return event.save().then(result => {
+                console.log(result);
+                return result;
+            }).catch(err => {
+                console.log(err);
+                throw err;
+            });
         }
      },
      graphiql: true    
   })
 );
 
+// Conectar ao banco de dados
+mongoose.connect('mongodb://mongodb:27017/events-react-dev', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() =>{
+    app.listen(3000);
+}).catch(err => {
+    console.log(err);
+});
 
-app.listen(3000);
+
