63ab18d74fea3ad6f2c4a6e9ebeab278e30b4452
Guilherme B.(gbmdevs) <guih.smi2@outlook.com>
2021-09-29 17:32:03 +0000

    Continuacao do Estudos GraphQL

--- GraphQL01/index.js
+++ GraphQL01/index.js
@@ -4,16 +4,35 @@ const { graphqlHTTP } = require('express-graphql');
 const { buildSchema}    = require("graphql");
 const app               = express();
 
-//app.use(bodyParser.json());
+// Bacalhau para Testes
+const events = [];
+
+app.use(bodyParser.json());
+
 app.use('/graphql',
    graphqlHTTP({
       schema: buildSchema(`
-          type RootQuery {
-              events: [String!]!
+        type Event { 
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+      
+        input EventInput{
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+              events: [Event!]!
           }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -23,7 +42,18 @@ app.use('/graphql',
      `),
      rootValue: {
         events: () => {
-            return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+            return events;
+        },
+        createEvent: args => {
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date().toISOString()
+            }
+            events.push(event);
+            return event
         }
      },
      graphiql: true    
