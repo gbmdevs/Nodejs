6ca9fb80ef8e5399c8d90d931d8d9b85f11b6d5f
Guilherme B.(gbmdevs) <guih.smi2@outlook.com>
2021-10-03 02:12:57 +0000

    Aula 6 - Inclusao da Relacao Usuario e Evento

--- GraphQL01/index.js
+++ GraphQL01/index.js
@@ -2,10 +2,13 @@ const express           = require("express");
 const bodyParser        = require("body-parser");
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema}    = require("graphql");
+const bcrypt            = require('bcryptjs');
 const app               = express();
 const mongoose          = require('mongoose');
 
+// Modelos
 const Event             = require('./models/event.js');
+const User              = require('./models/user.js');
 
 // Bacalhau para Testes
 const events = [];
@@ -25,6 +28,17 @@ app.use('/graphql',
             price: Float!
             date: String!
         }
+
+        type User { 
+            _id: ID!
+            email: String!
+            password: String
+        }
+
+        input UserInput{
+             email: String!,
+             password: String!
+        }
       
         input EventInput{
             title: String!
@@ -39,6 +53,7 @@ app.use('/graphql',
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema {
@@ -57,24 +72,55 @@ app.use('/graphql',
                 console.log(err);
             })
         },
+        createUser: args => { 
+            // Localizar caso já exista algum usuário cadastrado no sistema
+            return User.findOne({ email: args.userInput.email })
+            .then(user =>{
+                if(user){
+                    throw new Error('Usuário ja existente.');
+                }
+                return  bcrypt.hash(args.userInput.password, 12)
+            }).then(passwordEncrypted => {
+                const user = new User({
+                    email: args.userInput.email,
+                    password: passwordEncrypted
+                });
+                return user.save();
+             })
+             .then(result => {
+                return {...result._doc , password: null, _id: result.id }
+             })
+             .catch(err => {
+               throw err;
+            });
+        },
         createEvent: args => {
-            //const event = {
-            //    _id: Math.random().toString(),
-            //    title: args.eventInput.title,
-            //     description: args.eventInput.description,
-            //    price: +args.eventInput.price,
-            //     date: new Date().toISOString()
-            //}
             const event  = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
-                date: new Date(args.eventInput.date)
+                date: new Date(args.eventInput.date),
+                creator: '61590be7263fbf8b87aa8420'
+            });
+
+            let createdEvent;
+            return event
+            .save()
+            .then(result => {
+                createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+                return User.findById('61590be7263fbf8b87aa8420');
             })
-            return event.save().then(result => {
-                console.log(result);
-                return result;
-            }).catch(err => {
+            .then(user => {
+                if(!user){
+                    throw new Error('Usuário não encontrado.')
+                }
+                user.createdEvents.push(event);
+                return user.save();
+            })
+            .then(result => {
+                 return createdEvent;
+            })
+            .catch(err => {
                 console.log(err);
                 throw err;
             });
@@ -96,3 +142,22 @@ mongoose.connect('mongodb://mongodb:27017/events-react-dev', {
 });
 
 
+
+
+/*
+
+Criar um Registro de Exemplo
+
+mutation{createEvent(eventInput: {
+     title: "Harry potter",
+     description: "Um livro muito bom para ler.",
+     price: 85.99,
+     date: "2021-10-01T15:32:47.435Z"
+}){
+  title,
+  description
+}
+}
+
+
+*/
